e22c9e7fcad789f5b77d0aed91dc39486aaca4d9
nemilya <nemilya@gmail.com>
2017-12-05 16:16:35 +0500

    fix telegram recipe for correct mobile version

--- webview.js
+++ webview.js
@@ -1,15 +1,18 @@
+'use strict';
+
 const path = require('path');
 
-module.exports = (Franz) => {
+module.exports = Franz => {
   const getMessages = function getMessages() {
-    const search = document.querySelector('.im_dialogs_search_field').value;
-
     let count = 0;
-    if (search === '') {
-      const elements = document.querySelectorAll('.im_dialog_badge:not(.ng-hide)');
-      for (let i = 0; i < elements.length; i += 1) {
-        if (elements[i].innerHTML !== 0) {
-          count += 1;
+    const searchElement = document.querySelector('.im_dialogs_search_field');
+    if (searchElement && searchElement.value === '') {
+      const elements = document.querySelectorAll('.im_dialog_badge:not(.ng-hide):not(.im_dialog_badge_muted)');
+      if (elements) {
+        for (let i = 0; i < elements.length; i += 1) {
+          if (elements[i].innerHTML !== 0) {
+            count += 1;
+          }
         }
       }
     }
@@ -17,15 +20,11 @@ module.exports = (Franz) => {
     Franz.setBadge(count);
   };
 
+
+  if (localStorage.layout_selected == '"desktop"'){
+    document.querySelector('body').classList.add('isDesktop')
+  }
+
   Franz.injectCSS(path.join(__dirname, 'service.css'));
-
-  // css mixins for correct view in mobile or desktop layouts
-  if (localStorage.layout_selected == '"mobile"'){
-    Franz.injectCSS(path.join(__dirname, 'service_layout_mobile.css'));
-  }
-  else{
-    Franz.injectCSS(path.join(__dirname, 'service_layout_desktop.css'));
-  }
-
   Franz.loop(getMessages);
 };
